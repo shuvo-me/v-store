bbfcd135bf252b0955e614d7914c70715d4a7913
shuvo-me <shuvo.js.dev@gmail.com>
2025-08-12 11:08:40 +0600

    test(authstore): wrote basic test for auth store

---
+++ src/store/__tests__/auth.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import { setActivePinia, createPinia } from "pinia";
+import useAuthStore from "../auth";
+
+describe("Testing Auth store", () => {
+  let authStore: ReturnType<typeof useAuthStore>;
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    authStore = useAuthStore();
+  });
+
+  test("Auth store be defined", () => {
+    expect(authStore).toBeDefined();
+  });
+});
